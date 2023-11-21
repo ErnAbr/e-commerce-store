82ada66249208eabeb5661ace8493b30510460c0
ErnAbr <ern_abr@yahoo.com>
2023-11-21 10:19:41 +0200

    vite init and product catalog

--- ReStore/API/Program.cs
+++ ReStore/API/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+//adding CORS
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -22,6 +24,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
 
 app.UseAuthorization();
 

34a74c60df59ebfe121c765af62e1e64a1bae0f4
ErnAbr <ern_abr@yahoo.com>
2023-11-27 12:07:57 +0200

    redux basics

--- ReStore/client/src/index.tsx
+++ ReStore/client/src/index.tsx
@@ -9,10 +9,15 @@ import { router } from "./app/router/Routes.tsx";
 import { RouterProvider } from "react-router-dom";
 import { StoreProvider } from "./app/context/StoreContext.tsx";
 
+import { Provider } from "react-redux";
+import { store } from "./app/store/configureStore.ts";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <StoreProvider>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </StoreProvider>
   </React.StrictMode>
 );

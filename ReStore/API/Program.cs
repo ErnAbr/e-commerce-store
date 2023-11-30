59d7f17136e7ddc646ec0aecc72c89bb4d882245
ErnAbr <ern_abr@yahoo.com>
2023-11-30 09:53:46 +0200

    user roles and user entitity

--- ReStore/API/Program.cs
+++ ReStore/API/Program.cs
@@ -1,5 +1,7 @@
 using API.Data;
+using API.Entities;
 using API.Middleware;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +19,12 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 //adding CORS
 builder.Services.AddCors();
+builder.Services.AddIdentityCore<User>()
+  .AddRoles<IdentityRole>()
+  .AddEntityFrameworkStores<StoreContext>();
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
 
 var app = builder.Build();
 
@@ -40,11 +48,12 @@ app.MapControllers();
 
 var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
 var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
 try
 {
-    context.Database.Migrate();
-    DbInitializer.Initialize(context);
+    await context.Database.MigrateAsync();
+    await DbInitializer.Initialize(context, userManager);
 }
 catch (Exception ex)
 {

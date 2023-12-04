9d8ad32ea47ddba1f3dbfa9f0cb5f0ce6268a4e3
ErnAbr <ern_abr@yahoo.com>
2023-12-04 12:38:14 +0200

    OrderAggregate entities

--- ReStore/API/Data/StoreContext.cs
+++ ReStore/API/Data/StoreContext.cs
@@ -1,4 +1,5 @@
 using API.Entities;
+using API.Entities.OrderAggregate;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,7 @@ namespace API.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Basket> Baskets { get; set; }
+        public DbSet<Order> Orders { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
